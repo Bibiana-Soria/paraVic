c0239e6c6e7e9b11ac4b50490bb69105cf076d53
Bibiana Soria <bibiana_3141240181@utd.edu.mx>
2026-03-09 15:09:08 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+function mostrarMensaje(){
+
+    const mensajes = [
+        "Eres una persona muy especial",
+        "Nunca olvides lo increíble que eres ✨",
+        "El mundo es mejor porque existes",
+        "Siempre habrá alguien pensando en ti",
+        "Sigue brillando como lo haces siempre",
+        "Eres capaz de lograr cosas maravillosas",
+        "Iluminas mi vida",
+        "Te amooo",
+        "Te quiero mucho",
+        "Gracias por existir",
+        "Siempre estaré aquí para ti",
+        "I am proud of you",
+        "Te adoro",
+        "Te admiro",
+        "Te amo con todo mi corazón",
+        "Eres más que suficiente",
+        "Tu puedes mailov",
+        "Para ti mibida 🌹",
+        "Cuentas conmigo",
+        "Even on my worst, I'm best with you",
+        "Brillaaaaa corazonnn",
+        "Ya te dije que te amo?",
+        "Siempre contigo, aunque no esté ahí <3",
+        "Eres como un rayito de sol en los días frios",
+        "Abrazoooo 🫂",
+        "Me haces feliz",
+        "Eres increíble",
+        "Gracias por ser así como eres",
+        "Gracias por estar para mí",
+        "Y como dijeron los Perdidos: Don't you ever tell yourself that you're not enough",
+        "Nunca olvides lo especial que eres para mí"
+        ];
+
+    const indice = Math.floor(Math.random()*mensajes.length);
+
+    document.getElementById("mensaje").textContent = mensajes[indice];
+
+    document.getElementById("modal").style.display = "flex";
+}
+
+function cerrarModal(){
+    document.getElementById("modal").style.display = "none";
+}
